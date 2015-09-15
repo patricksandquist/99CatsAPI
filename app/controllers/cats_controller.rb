6e99fe0235d2eb435fc046a404d1bbc91444b554
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-15 14:48:27 -0700

    add edit view

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -9,4 +9,29 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     render :show
   end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat.id)
+    else
+      render :error
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def new
+    render :new
+  end
+
+  private
+  def cat_params
+    params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+  end
+
+
 end

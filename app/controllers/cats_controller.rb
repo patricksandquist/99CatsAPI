87cdcb9c10b5f58a9f87897a04c374225a5bfc1a
Patrick Sandquist <patricksandquist@gmail.com>
2015-09-15 15:37:39 -0700

    create partial for edit and new

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,11 +1,13 @@
 class CatsController < ApplicationController
 
   def index
+     
     @cats = Cat.all
     render :index
   end
 
   def show
+
     @cat = Cat.find(params[:id])
     render :show
   end
@@ -24,7 +26,17 @@ class CatsController < ApplicationController
     render :edit
   end
 
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update_attributes(cat_params)
+      render :show
+    else
+      render :error
+    end
+  end
+
   def new
+    @cat = Cat.new
     render :new
   end
 
